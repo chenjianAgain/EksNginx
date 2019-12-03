40489fd7879a3c0a6da9d4efb77a9a0453f4a6ac
Joseph <Joseph.siyi@gmail.com>
2019-12-04 00:28:08 +0800

    Added eks+nginx

--- bin/eks_nginx.ts
+++ bin/eks_nginx.ts
@@ -4,4 +4,9 @@ import cdk = require('@aws-cdk/core');
 import { EksNginxStack } from '../lib/eks_nginx-stack';
 
 const app = new cdk.App();
-new EksNginxStack(app, 'EksNginxStack');
+
+const env = {
+    region: app.node.tryGetContext('region') || process.env.CDK_INTEG_REGION || process.env.CDK_DEFAULT_REGION,
+    account: app.node.tryGetContext('account') || process.env.CDK_INTEG_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT
+};
+new EksNginxStack(app, 'EksNginxStack', { env });
