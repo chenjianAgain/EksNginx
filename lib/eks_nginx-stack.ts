64f3b402756380be5b13437e16e8f2c9e618f053
Joseph Chenkie <josephchenjian@icloud.com>
2019-12-04 08:07:59 +0800

    Update eks_nginx-stack.ts

--- lib/eks_nginx-stack.ts
+++ lib/eks_nginx-stack.ts
@@ -7,13 +7,8 @@ export class EksNginxStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // const vpc = ec2.Vpc.fromLookup(this, 'VPC', {
-    //   isDefault: true
-    // })
+    const vpc = new ec2.Vpc(this, 'vpc', { maxAzs: 2 });
 
-    const vpc = new ec2.Vpc(this, 'VPC', {
-      natGateways: 1
-    });
 
     const mastersRole = new iam.Role(this, 'AdminRole', {
       assumedBy: new iam.AccountRootPrincipal()
@@ -21,104 +16,16 @@ export class EksNginxStack extends cdk.Stack {
 
     const cluster = new eks.Cluster(this, 'EKSCluster', {
       vpc,
-      mastersRole
+      mastersRole,
+      defaultCapacity: 0,
     });
-
-//     const appLabel = { app: "nginx" };
-
-//     const ngxDeployment = {
-//       apiVersion: "apps/v1",
-//       kind: "Deployment",
-//       metadata: { name: "nginx" },
-//       spec: {
-//         replicas: 2,
-//         selector: { matchLabels: appLabel },
-//         template: {
-//           metadata: { labels: appLabel },
-//           spec: {
-//             containers: [
-//               {
-//                 name: "nginx",
-//                 image: "nginx"
-//               }
-//             ]
-//           }
-//         }
-//       }
-//     };
-
-//     const ngxSvc = {
-//       apiVersion: "v1",
-//       kind: "Service",
-//       metadata: { name: "nginx" },
-//       spec: {
-//         selector: {
-//           app: "nginx"
-//         },
-//         ports: [
-//           {
-//             name: "web",
-//             port: 80,
-//             targetPort: 80
-//           }
-//         ],
-//         type: "LoadBalancer"
-//       }
-//     }
-
-//     const caddyDeployment = {
-//       apiVersion: "apps/v1",
-//       kind: "Deployment",
-//       metadata: { name: "caddy" },
-//       spec: {
-//         replicas: 2,
-//         selector: { matchLabels: { app: "caddy" } },
-//         template: {
-//           metadata: { labels: { app: "caddy" } },
-//           spec: {
-//             containers: [
-//               {
-//                 name: "caddy",
-//                 image: "abiosoft/caddy"
-//               }
-//             ]
-//           }
-//         }
-//       }
-//     };
-
-//     const caddySvc = {
-//       apiVersion: "v1",
-//       kind: "Service",
-//       metadata: { name: "caddy" },
-//       spec: {
-//         selector: {
-//           app: "caddy"
-//         },
-//         ports: [
-//           {
-//             name: "caddy",
-//             port: 80,
-//             targetPort: 2015
-//           }
-//         ],
-//         type: "LoadBalancer"
-//       }
-//     };
-
-
-//     const manifest = [
-//       caddyDeployment,
-//       caddySvc
-//     ]
-
-//     new eks.KubernetesResource(this, 'MyCustomResource', {
-//       cluster,
-//       manifest
-//     })
-
-//     cluster.addResource('MyResources', ngxDeployment, ngxSvc);
-//     // cluster.addResource('MyResources', ngxDeployment, ngxSvc);
+    
+    // add some capacity to the cluster. The IAM instance role will
+    // automatically be mapped via aws-auth to allow nodes to join the cluster.
+    this.cluster.addCapacity('Nodes', {
+      instanceType: new ec2.InstanceType('t2.medium'),
+      desiredCapacity: 2,
+    });
 
   }
 }
